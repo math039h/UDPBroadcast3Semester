d3a3b6d56c53e8995419a0c710a7a31a8cde1a25
mathias brink <mathiasbrink@live.dk>
2019-12-11 14:15:04 +0100

    moved something

---
+++ App1/ViewModel/JoystickVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Model;
+
+namespace App1.ViewModel
+{
+    class JoystickVM : JoyStick
+    {
+
+    }
+}
